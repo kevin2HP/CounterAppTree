c63861a82572a174cafd40536e1445bbc95b85e9
kevin2HP <49846996+kevin2HP@users.noreply.github.com>
2020-07-16 10:27:04 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var i=0; 
+function add() {           
+    i++;       
+    document.getElementById('number').innerHTML=i;    
+}
+function minus(){
+    i--;
+    document.getElementById('number').innerHTML=i;    
+
+}
